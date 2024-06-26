de293f581da544e4fde7f18d1988ee91cea8c3c3
shubham <shubhamrupapara993@gmail.com>
2024-06-26 23:35:25 +0530

    feature(configuration section): Worked on model of image configuration and store image in to db

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@/db";
 
 const f = createUploadthing();
 
@@ -11,7 +13,29 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
-      return { configId };
+
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+
+      const imgMetaData = await sharp(buffer).metadata();
+      const { height, width } = imgMetaData;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: { id: configId },
+          data: { croppedImageUrl: file.url },
+        });
+        return { configId: updatedConfiguration.id };
+      }
     }),
 } satisfies FileRouter;
 
